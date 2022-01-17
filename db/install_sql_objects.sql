3b2015db45334c3dc6faa1b004a0fdd6e68aba70
izaowl <izasow@yahoo.com>
2022-01-17 10:27:15 +0000

    updated sql file

--- db/install_sql_objects.sql
+++ db/install_sql_objects.sql
@@ -1,4 +1,1 @@
-CREATE DATABASE message;
-CREATE DATABASE message_test;
--- in both databases execute this command:
 CREATE TABLE messages (id SERIAL PRIMARY KEY,  message VARCHAR(240), ch_user VARCHAR(30), created_at TIMESTAMP);\q
